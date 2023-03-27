6884138b3c41dd3c26a73fe41a15f27ccca75f52
Phinatech <ugbojudithchinemerem@gmail.com>
2023-03-27 12:49:56 +0100

    adminDash

--- client_side/src/App.tsx
+++ client_side/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Alloutes from "./Components/AllRoutes/Alloutes";
+import Admindashboard from "./Components/AllRoutes/AdminDashBoard";
 
 function App() {
   return (
     <div className="App">
       <Alloutes />
+      <Admindashboard />
     </div>
   );
 }
